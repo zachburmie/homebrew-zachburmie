cf1dda50e518e26dcb8062657f1042fc704a4df7
Zach Burmeister <45673140+zachburmie@users.noreply.github.com>
2019-04-05 17:48:08 -0600

    Update atx.rb

--- Formula/atx.rb
+++ Formula/atx.rb
@@ -1,8 +1,8 @@
 class Kns < Formula
   desc "Quick AWS role Switcher"
   homepage "https://github.com/zachburmie/atx"
-  url "https://github.com/zachburmie/atx.git", :revision => "10c8282ce05efcae1151794f7c220c4716bd2514"
-  version "10c8282ce05efcae1151794f7c220c4716bd2514"
+  url "https://github.com/zachburmie/atx.git", :revision => "613261c0efa388ab9e34cce50fa56f0e6b39366d"
+  version "613261c0efa388ab9e34cce50fa56f0e6b39366d"
   head "https://github.com/zachburmie/atx.git"
 
   depends_on "fzf"
