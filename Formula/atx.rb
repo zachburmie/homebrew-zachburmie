613261c0efa388ab9e34cce50fa56f0e6b39366d
Zach Burmeister <zach.burmeister@sendgrid.com>
2019-04-05 17:47:09 -0600

    versioning

--- Formula/atx.rb
+++ Formula/atx.rb
@@ -1,7 +1,7 @@
 class Kns < Formula
   desc "Quick AWS role Switcher"
   homepage "https://github.com/zachburmie/atx"
-  url "https://github.com/zachburmie/atx.git", :revision => "9e30d8df96dc864383845ea5a12cd460f33166fa"
+  url "https://github.com/zachburmie/atx.git", :revision => "10c8282ce05efcae1151794f7c220c4716bd2514"
   version "10c8282ce05efcae1151794f7c220c4716bd2514"
   head "https://github.com/zachburmie/atx.git"
 
