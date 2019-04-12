3e92c7682b6dcabaefa54b2a600370a537768655
Zach Burmeister <zach.burmeister@sendgrid.com>
2019-04-12 16:48:36 -0600

    Updates

--- Formula/atx.rb
+++ Formula/atx.rb
@@ -2,12 +2,12 @@ class Atx < Formula
   desc "Quick AWS role Switcher"
   homepage "https://github.com/zachburmie/atx"
   url "https://github.com/zachburmie/atx.git"
-  version "613261c0efa388ab9e34cce50fa56f0e6b39366d"
+  version "95cf5168f616eebfe42b0be210cc98850a712cde"
   head "https://github.com/zachburmie/atx.git"
 
+  depends_on "aws-okta"
   depends_on "fzf"
-  depends_on "aws-okta" => :optional
-
+  
   def install
     bin.install "bin/atx"
     prefix.install "helpers"
